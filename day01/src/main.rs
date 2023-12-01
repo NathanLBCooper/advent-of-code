8b84a7c43c19fad1439c27ca5ff13787ea440540
Nathan Cooper <nathan.lb.cooper@outlook.com>
2023-12-01 22:02:10 +0100

    Solve day one part two

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -35,22 +35,17 @@ mod calibration {
     }
 
     fn sum_line(line: &str) -> Result<i32> {
-        let is_digit = |c: &char| c.is_digit(10);
-
-        let first_digit = match line.chars().find(is_digit) {
-            Some(v) => v.clone(),
+        let first_digit = match find_first_digit(line) {
+            Some(v) => v,
             None => return Err(Error::new("can't find first digit")),
         };
 
-        let second_digit = match line.chars().rfind(is_digit) {
-            Some(v) => v.clone(),
+        let second_digit = match find_last_digit(line) {
+            Some(v) => v,
             None => return Err(Error::new("can't find second digit")),
         };
 
-        let appended_digits: String = [first_digit, second_digit].iter().collect();
-
-        let sum: i32 = appended_digits.parse::<i32>().unwrap();
-        return Ok(sum);
+        return Ok((first_digit * 10) + second_digit);
     }
 
     fn read_lines(filename: &str) -> Result<Vec<String>> {
@@ -59,17 +54,79 @@ mod calibration {
             Err(_) => Err(Error::new("can't read file")),
         };
     }
+
+    const NUMBERS: [&str; 9] = [
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    ];
+
+    fn find_first_digit(line: &str) -> Option<i32> {
+        for i in 0..line.chars().count() {
+            let remaining_text = &line[i..];
+
+            let leading_char = remaining_text.chars().nth(0).unwrap();
+            if leading_char.is_digit(10) {
+                let digit = leading_char.to_digit(10).unwrap() as i32;
+                return Some(digit);
+            }
+
+            for (j, number) in NUMBERS.iter().enumerate() {
+                let number_length = number.chars().count();
+                let remaining_length = remaining_text.chars().count();
+                if number_length > remaining_length {
+                    continue;
+                }
+
+                let text_to_compare = remaining_text[..number_length].to_string();
+
+                if number.eq(&text_to_compare) {
+                    return Some((j + 1) as i32);
+                }
+            }
+        }
+
+        return None;
+    }
+
+    // todo repetative
+    fn find_last_digit(line: &str) -> Option<i32> {
+        for i in 0..line.chars().count() {
+            let remaining_text = &line[..line.chars().count() - i];
+
+            let trailing_char = remaining_text.chars().last().unwrap();
+            if trailing_char.is_digit(10) {
+                let digit = trailing_char.to_digit(10).unwrap() as i32;
+                return Some(digit);
+            }
+
+            for (j, number) in NUMBERS.iter().enumerate() {
+                let number_length = number.chars().count();
+                let remaining_length = remaining_text.chars().count();
+                if number_length > remaining_length {
+                    continue;
+                }
+
+                let text_to_compare =
+                    remaining_text[remaining_text.chars().count() - number_length..].to_string();
+
+                if number.eq(&text_to_compare) {
+                    return Some((j + 1) as i32);
+                }
+            }
+        }
+
+        return None;
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::calibration::*;
+    use crate::calibration;
 
     #[test]
     fn can_calculate_sum_of_part_one_example_document() {
         let example_file = String::from("./example_calibration_part_one.txt");
 
-        let result = calculate_sum_of_calibration_document(&example_file);
+        let result = calibration::calculate_sum_of_calibration_document(&example_file);
 
         assert_eq!(result.is_err(), false);
         assert_eq!(result.unwrap(), 142);
@@ -79,7 +136,7 @@ mod tests {
     fn can_calculate_sum_of_part_two_example_document() {
         let example_file = String::from("./example_calibration_part_two.txt");
 
-        let result = calculate_sum_of_calibration_document(&example_file);
+        let result = calibration::calculate_sum_of_calibration_document(&example_file);
 
         assert_eq!(result.is_err(), false);
         assert_eq!(result.unwrap(), 281);
