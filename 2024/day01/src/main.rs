4e9f952be3314b35334aa83fe07c09e2500683d1
nathanlbcooper <nathan.lb.cooper@outlook.com>
2024-12-04 14:08:55 +0100

    day 01 2024 part 2

--- 2024/day01/src/main.rs
+++ 2024/day01/src/main.rs
@@ -11,18 +11,43 @@ fn main() {
         }
     };
 
-    let (mut first_numbers, mut second_numbers): (Vec<i32>, Vec<i32>) = lines.iter().map(|(f, s)| (*f, *s)).unzip();
+    let (first_numbers, second_numbers): (Vec<i32>, Vec<i32>) = lines.iter().map(|(f, s)| (*f, *s)).unzip();
 
-    first_numbers.sort();
-    second_numbers.sort();
+    println!("Total distance: {}", part_one(first_numbers.as_slice(), second_numbers.as_slice(), lines.len()));
+    println!("Similarity: {}", part_two(first_numbers.as_slice(), second_numbers.as_slice()));
+}
+
+fn part_one(first_numbers: &[i32], second_numbers: &[i32], length: usize) -> i32 {
+    let mut sorted_first_numbers = first_numbers.to_vec();
+    sorted_first_numbers.sort();
+    let mut sorted_second_numbers = second_numbers.to_vec();
+    sorted_second_numbers.sort();
 
     let mut total_distance = 0;
-    for i in 0..lines.len() {
-        let distance = (first_numbers[i] - second_numbers[i]).abs();
+    for i in 0..length {
+        let distance = (sorted_first_numbers[i] - sorted_second_numbers[i]).abs();
         total_distance += distance;
     }
 
-    println!("Total distance: {}", total_distance);
+    total_distance
+}
+
+fn part_two(first_numbers: &[i32], second_numbers: &[i32]) -> i32 {
+    let mut second_number_counts = std::collections::HashMap::new();
+    for i in second_numbers {
+        let count = second_number_counts.entry(i).or_insert(0);
+        *count += 1;
+    }
+
+    let mut similarity = 0;
+    for i in first_numbers {
+        match second_number_counts.get(i) {
+            None => {}
+            Some(c) => { similarity += i * c }
+        }
+    }
+
+    similarity
 }
 
 fn parse_file() -> Result<Vec<(i32, i32)>, AdventError> {
